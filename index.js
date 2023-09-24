002159e9e6021f489bc7ebadffc636eaed645520
Miracle8221 <anagwumiracle098@gmail.com>
2023-09-24 18:39:21 +0100

    Finish setting the game ready to play

--- index.js
+++ index.js
@@ -70,5 +70,41 @@ const playGame = () => {
             } 
         }
     }
-    
+    // function to run when the game is over
+    const gameOver = (playerOptions,moveLeft) => {
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+        chooseMove.innerText = 'Game Over!!'
+        movesLeft.style.display = 'none';
+
+        if(playerScore > computerScore){
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Won The Game'
+            result.style.color = '#308D46';
+        }
+        else if(playerScore < computerScore){
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Lost The Game';
+            result.style.color = 'red';
+        }
+        else{
+            result.style.fontSize = '2rem';
+            result.innerText = 'Tie';
+            result.style.color = 'grey'
+        }
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display = 'flex'
+        reloadBtn.addEventListener('click',() => {
+            window.location.reload();
+        })
+    } 
+    // calling playgame function inside game
+    playGame();
+
 }
+// callling game function
+game();
