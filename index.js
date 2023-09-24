9a7d6bd3c0d934068b1986da9abc92f068f9f648
Miracle8221 <anagwumiracle098@gmail.com>
2023-09-24 18:07:22 +0100

    To create the winner

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const game = () => {
 const playGame = () => {
     const rockBtn = document.querySelector('.rock');
     const paperBtn = document.querySelector('.paper');
-    const scissorBtn = document.querySelector('.scissor');
+    const scissorBtn = document.querySelector('.scissors');
     const playerOptions = [rockBtn,paperBtn,scissorBtn];
     const computerOptions = ['rock','paper','scissors']
     // function to start playing game
@@ -25,6 +25,50 @@ const playGame = () => {
             }
         })
     })
-    } 
-        
+    }
+    // function to decide winner
+    const winner = (player,computer) => {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('.p-count');
+        const computerScoreBoard = document.querySelector('.c-count');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
+        if(player === computer){
+            result.textContent = 'Tie'
+        }
+        else if (player == 'rock'){
+            if (compter == 'paper'){
+                result.textContent = 'Computer won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }else{
+                result.textContent = 'Player won'
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
+        else if (player == 'scissors'){
+            if(computer == 'rock'){
+                result.textContent = 'Computer won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }else{
+                result.textContent = 'Player won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
+        else if(player == 'paper'){
+            if(computer == 'scissors'){
+                result.textContent = 'Computer won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }else{
+                result.textContent = 'Player won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            } 
+        }
+    }
+    
 }
