c3f871f57fa2b61eece3327c1eb97b56543d8301
Miracle8221 <anagwumiracle098@gmail.com>
2023-09-24 17:36:53 +0100

    creat function to  start playing game

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const playGame = () => {
     const scissorBtn = document.querySelector('.scissor');
     const playerOptions = [rockBtn,paperBtn,scissorBtn];
     const computerOptions = ['rock','paper','scissors']
+    // function to start playing game
+    playerOptions.forEach(Option => {
+        Option.addEventListener('click',function(){
+            const moveLeft = document.querySelector('.movesleft');
+            moves++;
+            movesLeft.innerText = `Moves Left: ${10-moves}`;
+            const choiceNumber = Math.floor(Math.random() * 3);
+            const computerChoice = computerOptions[choiceNumber];
+            // function to check who wins
+            winner(this.innerText,computerChoice)
+            // calling gameover function after 10 moves
+            if(moves == 10){
+                gameOver(playerOptions,movesLeft);
+            }
+        })
+    })
     } 
         
 }
